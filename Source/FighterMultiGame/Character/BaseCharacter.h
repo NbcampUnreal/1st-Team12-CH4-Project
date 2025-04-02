ca9497db5f726da79d7e0982ac588bb21f55769b
micalia <60741767+micalia@users.noreply.github.com>
2025-04-02 17:35:04 +0900

    Add 손오공 캐릭터 모델링 추가 및 TPC 템플릿 캐릭터 추가

--- Source/FighterMultiGame/Character/BaseCharacter.h
+++ Source/FighterMultiGame/Character/BaseCharacter.h
@@ -21,18 +21,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	virtual void BaseAttack() PURE_VIRTUAL(ABaseCharacter::BaseAttack);
-	virtual void SpecialAttack() PURE_VIRTUAL(ABaseCharacter::SpecialAttack);
-
-	virtual void Move() PURE_VIRTUAL(ABaseCharacter::Move);
-	virtual void Jump() PURE_VIRTUAL(ABaseCharacter::Jump);
-
-	//void GetItem();
-	virtual void Damaged() PURE_VIRTUAL(ABaseCharacter::Damaged);
-	virtual void Die() PURE_VIRTUAL(ABaseCharacter::Die);
-	
 private:
-	int32 Hp;
 	int32 Power;
 
 	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess=true))
---
+++ Source/FighterMultiGame/Character/WukongCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "WukongCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIGHTERMULTIGAME_API AWukongCharacter : public ATP_ThirdPersonCharacter
+{
+	GENERATED_BODY()
+	
+};
